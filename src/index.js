7401ec139d6ced9ef5dd5ce890bc018b83dd52f3
Mikalai Stadub <mikalaistadub@gmail.com>
2019-03-24 15:08:01 +0300

    brackets done :)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  while (str != '') {
+    let next = str.split('()').join('').split('[]').join('').split('{}').join('').split('||').join('').split('12').join('').split('34').join('').split('56').join('').split('77').join('').split('88').join('');
+    console.log(next == str);
+    
+    if (next == str) {
+      return false 
+    }
+    str = next
+  }
+  return true 
 }
